d2e2d8200dfc2b6b7575960aba9fb699f605a4fb
shellshock dNull <shellshock1953@gmail.com>
2016-12-30 00:29:06 +0200

    at last, single intanse of board for every player

--- oop/battle_v3.py
+++ oop/battle_v3.py
@@ -1,21 +1,14 @@
 import string
 
-
-
 class Board():
-    board = []
-    size = 10
-    
-    def generate(self):
+    def __init__(self):
+        self.size = 10
+        self.board = []
         for sell in range(10):
            self.board.append(["."] * 10)
-        return self.board
 
-    def refresh(self):
-        print self.navy['2sell_a'].health()
-        print self.navy['2sell_a'].ship
-
-    def show(self):
+    def show(self, username):
+        print "     USER: %s" % username
         alphabet = string.uppercase[:self.size]
         numbers = ' '.join(str(x) for x in range(1,11))
         print "     %s" % numbers
@@ -24,7 +17,29 @@ class Board():
             board_row = " ".join(self.board[row])
             print "   %s %s" % (alphabet[letter],board_row)
             letter += 1
+
+    def layout(self, ship, x, y):
+        if ship.layout == 'horizontal':
+            cur_x = x
+            cur_y = y + 1
+        if ship.layout == 'vertical':
+            cur_x = x + 1
+            cur_y = y
+        return cur_x, cur_y
+
+    def draw_ships(self, navy):
+        for ship in navy.values():
+            x = ship.x
+            y = ship.y
+            leng = ship.leng
+            layout = ship.layout
+            for sell in range(leng):
+                if ship.layout == 'vertical':
+                    self.board[x][y + sell] = '='
+                if ship.layout == 'horisontal':
+                    self.board[x + sell][y] = '='
             
+    def place_ships(self, navy):
 
 class Ship():
     body = '='
@@ -52,23 +67,36 @@ class Ship():
         elif self.body not in self.ship:
             state =  "Health - [ DEAD ]"
         return state
-        
 
-class Player(Board, Ship):
-    def __init__(self, name):
-        self.name = name
-        print ("Hello %s" % name)
+        
+class Player():
+    def __init__(self):
+        self.name = raw_input("Enter name: ")
+        print ("Hello %s" % self.name)
+
+        self.board = Board()
 
     navy = {
         '4sell_a' : Ship(1,1,4,'vertical'),
         '2sell_a' : Ship(3,1,2,'horisontal'),
-        '2sell_b' : Ship(5,1,2,'horisontal'),
+        # '2sell_b' : Ship(5,1,2,'horisontal'),
     }
 
+    def draw_ships(self):
+        self.board.draw_ships(self.navy)
+
+
+class Game():
+    def __init__(self):
+        self.player1 = Player()
+        self.player2 = Player()
+        self.player1.draw_ships()
+
+    def show_gamefield(self):
+        self.player1.board.show(self.player1.name)
+        self.player2.board.show(self.player2.name)
+
+
 if __name__ == '__main__':
-    player = Player('shellshock')
-    player.generate()
-    player.navy['2sell_a'].hit(0)
-    player.navy['2sell_a'].hit(1)
-    player.show()
-    player.refresh()
+    game = Game()
+    game.show_gamefield()
